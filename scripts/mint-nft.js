1a709f1abb6a08755e218039122ebcd6103efde1
0xSumna <notes@sigil.systems>
2021-10-13 00:57:01 -0500

    updates to mint script

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -2,4 +2,7 @@ require('dotenv').config();
 const API_URL = process.env.API_URL;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(API_URL);
-const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+const contract = require("../artifacts/contracts/YapeNFT.sol/YapeNFT.json");
+
+
+
